ecf74d7432ee6f66fd8314a8f1ceec0a2247b52d
Abirvab Deb <adeb@concertai.com>
2021-07-15 15:22:20 -0400

    cookiecutter data-science repo structure; can remove stuff as needed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='May 2021 Cohort Capstone work for FourthBrain's AI/MLE training program',
+    author='Abirvab Deb, Leon Adams, Aisha Ali',
+    license='BSD-3',
+)
